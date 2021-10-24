e004cc97081129f68d8482b5edc12aa761d571ee
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 13:57:05 +0200

    comment bolletta.h

--- Bolletta/bolletta.h
+++ Bolletta/bolletta.h
@@ -5,26 +5,36 @@
 
 class bolletta {
 private:
+    // Nested list class
     class nodo {
     public:
-        // nodo();
+        // nodo(); DEPRECATED
+        // Default node constructor
         nodo(const telefonata&, nodo*);
+        // List elements
         telefonata info;
         nodo* next;
-
+        // Node destructor
         ~nodo();
     };
 
+    // First node in the list
     nodo* first;
+
+    // Copies node
     static nodo* copia(nodo*);
+    // Destroys node
     static void distruggi(nodo*);
 
 public:
+    // Nested iterator class
     class iteratore {
         friend class bolletta;
     private:
+        // Points at node
         bolletta::nodo* punt;
     public:
+        // Misc operator overloading
         bool operator ==(const iteratore&) const;
         bool operator !=(const iteratore&) const;
         iteratore& operator ++();
@@ -33,23 +43,37 @@ public:
         telefonata& operator *() const;
     };
 
+    // Constructors
     bolletta();
     bolletta(const bolletta&);
+
+    // Checks if list is empty
     bool Vuota() const;
+
+    // Adds phone call
     void Aggiungi_Telefonata(const telefonata&);
+    // Removes phone call
     void Togli_Telefonata(const telefonata&);
+
+    // Extracts a phone call
     telefonata Estrai_Una();
 
+    // Misc operator overloading
     bolletta& operator =(const bolletta&);
     telefonata& operator [](const iteratore&) const;
 
+    // Destructor
     ~bolletta();
 
+    // Indicates list stard and end for iterator
     iteratore begin() const;
     iteratore end() const;
 };
 
+// Sums call lengths using iterator
 orario Somma_Durate(const bolletta&);
+
+// Returns calls from phone number
 bolletta Chiamate_A(int, bolletta&);
 
 #endif
