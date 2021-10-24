c352488d17d332dd837f8b87608699397d949a96
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 18:05:59 +0200

    comment telefonata.h

--- Bolletta/telefonata.h
+++ Bolletta/telefonata.h
@@ -6,19 +6,28 @@
 
 class telefonata {
 private:
+	// Call start timestamp
 	orario inizio;
+	// Call end timestamp
 	orario fine;
+	// Phone number
 	int numero;
 
 public:
+	// Constructors
 	telefonata();
 	telefonata(const orario& =0, const orario& =0, const int& =0);
+	// Returns start timestamp
 	orario Inizio() const;
+	// Returns end timestamp
 	orario Fine() const;
+	// Returns phone number
 	int Numero() const;
+	// == overloading
 	bool operator ==(const telefonata&) const;
 };
 
+// << overloading for formatted object print output
 std::ostream& operator <<(std::ostream&, const telefonata&);
 
 
