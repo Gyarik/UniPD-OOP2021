f835af9ac8e31cc479003bb9893ae61b1835b838
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 18:01:46 +0200

    comment bolletta.cpp

--- Bolletta/bolletta.cpp
+++ Bolletta/bolletta.cpp
@@ -6,14 +6,17 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+// Node constructor
 bolletta::nodo::nodo(const telefonata& t, nodo* s) : info(t), next(s) {}
 
 bolletta::nodo::~nodo() {
+    // Deletes pointer to next node as long as it isn't null (last node)
     if(next != nullptr)
         delete next;
 }
 
 bolletta::nodo* bolletta::copia(nodo* p) {
+    // If node exists return copy of parameter
     if(!p)
         return 0;
     else
@@ -21,6 +24,7 @@ bolletta::nodo* bolletta::copia(nodo* p) {
 }
 
 void bolletta::distruggi(nodo* p) {
+    // If node exists destroy pointer to next node, then node
     if(p) {
         distruggi(p->next);
         delete p;
@@ -28,20 +32,24 @@ void bolletta::distruggi(nodo* p) {
 }
 
 bool bolletta::iteratore::operator ==(const iteratore& i) const {
+    // Logic comparison between pointers
     return punt == i.punt;
 }
 
 bool bolletta::iteratore::operator !=(const iteratore& i) const {
+    // Ditto
     return punt != i.punt;
 }
 
-bolletta::iteratore& bolletta::iteratore::operator ++() { // ++var
+bolletta::iteratore& bolletta::iteratore::operator ++() {   // ++var
+    // Increments, then returns invoked object
     if(punt)
         punt = punt->next;
     return *this;
 }
 
 bolletta::iteratore bolletta::iteratore::operator ++(int) { // var++
+    // aux to return original value first, then increment
     iteratore aux = *this;
     if(punt)
         punt = punt->next;
@@ -49,33 +57,40 @@ bolletta::iteratore bolletta::iteratore::operator ++(int) { // var++
 }
 
 telefonata* bolletta::iteratore::operator ->() const {
+    // Makes iteration more readable through -> operator
     telefonata* aux;
     aux = new telefonata(punt->info);
     return aux;
 }
 
 telefonata& bolletta::iteratore::operator *() const {
+    // Ditto but with * operator
     return punt->info;
 }
 
+// Constructors
 bolletta::bolletta() : first(nullptr) {}
-
 bolletta::bolletta(const bolletta& b) : first(copia(b.first)) {}
 
 bool bolletta::Vuota() const {
+    // Checks if null
     return first == 0;
 }
 
 void bolletta::Aggiungi_Telefonata(const telefonata& t) {
+    // Adds parameter (phone call) to list
     first = new nodo(t, first);
 }
 
 void bolletta::Togli_Telefonata(const telefonata& t) {
+    // Removes parameter (phone call) from list
     nodo* p = first, *prec = nullptr;
+    // If node exists and calls don't match, shift list
     while(p && !(p->info == t)) {
         prec = p;
         p = p->next;
     }
+    // Final checks before deleting call
     if(p) {
         if(!prec)
             first = p->next;
@@ -86,6 +101,7 @@ void bolletta::Togli_Telefonata(const telefonata& t) {
 }
 
 telefonata bolletta::Estrai_Una() {
+    // Extracts call from first node
     nodo* p = first;
     telefonata aux = p->info;
     delete p;
@@ -93,6 +109,7 @@ telefonata bolletta::Estrai_Una() {
 }
 
 bolletta& bolletta::operator =(const bolletta& b) {
+    // If objects are not already equal, copy from first node
     if(this != &b) {
         distruggi(first);
         first = copia(b.first);
@@ -101,21 +118,25 @@ bolletta& bolletta::operator =(const bolletta& b) {
 }
 
 telefonata& bolletta::operator [](const bolletta::iteratore& it) const {
+    // Makes iteration easier with array-like properties
     return (it.punt)->info;
 }
 
 bolletta::~bolletta() {
+    // Deletes first node
     if(first)
         delete first;
 }
 
 bolletta::iteratore bolletta::begin() const {
+    // Returns list start
     bolletta::iteratore aux;
     aux.punt = first;
     return aux;
 }
 
 bolletta::iteratore bolletta::end() const {
+    // Returns list end + 1 (nullptr)
     bolletta::iteratore aux;
     aux.punt = nullptr;
     return aux;
@@ -133,6 +154,7 @@ bolletta::iteratore bolletta::end() const {
 
 orario Somma_Durate(const bolletta& b) {
     orario durata;
+    // Three ways of summing using operator overloading ([], *, ->)
     for(bolletta::iteratore it=b.begin(); it!=b.end(); ++it)
         // durata = durata + (b[it].Fine() - b[it].Inizio());
         // durata = durata + ((*it).Fine() - (*it).Inizio());
@@ -142,6 +164,7 @@ orario Somma_Durate(const bolletta& b) {
 
 bolletta Chiamate_A(int num, bolletta& b) {
     bolletta selezionate, resto;
+    // If list is not empty, extract all calls from one number
     while(!b.Vuota()) {
         telefonata t = b.Estrai_Una();
         if(t.Numero() == num)
@@ -149,6 +172,7 @@ bolletta Chiamate_A(int num, bolletta& b) {
         else
             resto.Aggiungi_Telefonata(t);
     }
+    // Replace parameter object with remainder using overloaded =
     b = resto;
     return selezionate;
 }
