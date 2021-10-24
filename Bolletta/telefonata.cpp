836523f70bc86ed59b61c5ba9038eb040b2b10aa
BuildTools <unconfigured@null.spigotmc.org>
2021-10-24 18:08:12 +0200

    comment telefonata.cpp

--- Bolletta/telefonata.cpp
+++ Bolletta/telefonata.cpp
@@ -5,9 +5,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-
+// Constructors
 telefonata::telefonata(const orario& i, const orario& f, const int& n) : inizio(i), fine(f), numero(n) {}
-
 telefonata::telefonata() : numero(0) {}
 
 orario telefonata::Inizio() const {
@@ -23,10 +22,12 @@ int telefonata::Numero() const {
 }
 
 bool telefonata::operator ==(const telefonata& t) const {
+	// Checks if all parameters are equal
 	return inizio==t.inizio && fine==t.fine && numero==t.numero;
 }
 
 std::ostream& operator <<(std::ostream& s, const telefonata& t) {
+	// Cool formatting
 	s << "Inizio: " << t.Inizio() << endl << "Fine: " << t.Fine() << endl << "Numero Chiamato: " << t.Numero();
 	return s;
 }
